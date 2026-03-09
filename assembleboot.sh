36bc668fc7df1c7e450023c31fd966c44c64aa0d
Giovanni Dueck <giodueck@gmail.com>
2026-03-08 21:46:33 -0300

    Fix assemble script, add one each for boot and program to resolve labels correctly

--- assembleboot.sh
+++ assembleboot.sh
@@ -9,5 +9,6 @@ if [ $# -lt 2 ]; then
     exit 1
 fi
 
-riscv32-unknown-elf-as --march=rv32i $SOURCE -o $OUTNAME
+riscv32-unknown-elf-as --march=rv32i $SOURCE -o $SOURCE.o
+riscv32-unknown-elf-ld -Ttext=0x1000 $SOURCE.o -o $OUTNAME
 riscv32-unknown-elf-objcopy -O binary $OUTNAME $OUTNAME.bin
---
+++ assembleprog.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+SOURCE=$1
+OUTNAME=$2
+
+if [ $# -lt 2 ]; then
+    echo "Want at least 2 arguments: SOURCE and OUTNAME"
+    exit 1
+fi
+
+riscv32-unknown-elf-as --march=rv32i $SOURCE -o $SOURCE.o
+riscv32-unknown-elf-ld -Ttext=0x2000 $SOURCE.o -o $OUTNAME
+riscv32-unknown-elf-objcopy -O binary $OUTNAME $OUTNAME.bin
